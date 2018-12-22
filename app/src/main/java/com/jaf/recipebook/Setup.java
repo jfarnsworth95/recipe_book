2fcfea53a435480fb4ae36db93fe085e53e81528
jfarnsworth95 <jafarns95@gmail.com>
2018-12-22 16:12:14 -0600

    Added basics of google api sign in and service check, requires listeners (TBA)

--- app/src/main/java/com/jaf/recipebook/Setup.java
+++ app/src/main/java/com/jaf/recipebook/Setup.java
@@ -1,25 +1,37 @@
 package com.jaf.recipebook;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Environment;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import java.io.File;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveResourceClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Setup extends AppCompatActivity {
 
     public final static String STORAGE_OPTION = "com.jaf.recipebook.STORAGE_OPTION";
+    final int GOOGLE_SIGN_IN_ACTIVITY = 1000; //Int that represents google sign in activity
+    final int GOOGLE_SERVICES_CHECK = 1001; //Int that represents google service error dialog
     final int PER_WRITE_EXTERNAL_STORAGE = 1;
     int grantForWrite = 0;
+
+    final String TAG = "Setup";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,7 @@ public class Setup extends AppCompatActivity {
         view.postDelayed(new Runnable(){
             public void run(){
                 getPermissions();
+                accessGoogleDrive();
             }
         },2000);
     }
@@ -77,5 +90,50 @@ public class Setup extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    /**
+     * To allow Google Drive integration, check that Google Play API is up to date/active
+     */
+    public void accessGoogleDrive(){
+
+        //Check if Google Play Services is active
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int returnCode = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        Dialog errorDialog = googleApiAvailability.getErrorDialog(this,returnCode,GOOGLE_SERVICES_CHECK);
+        if(errorDialog != null) { //null if success
+            errorDialog.show(); //Request user to update/install services
+        }
+
+        //Request access to Google Drive
+        // This account must have the necessary scopes to make the API call
+        // See https://developers.google.com/identity/sign-in/android/
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        //What do if account is null?
+        if(account == null){
+            // Configure sign-in to request the user's ID, email address, and basic
+            // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build();
+
+            // Build a GoogleSignInClient with the options specified by gso.
+            GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+            //Sign in to Google Services
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, GOOGLE_SIGN_IN_ACTIVITY);
+        }
+
+        // Get the app's Drive folder
+        DriveResourceClient client = Drive.getDriveResourceClient(this, account);
+        client.getAppFolder()
+        .addOnCompleteListener(this, new OnCompleteListener<DriveFolder>() {
+            @Override
+            public void onComplete(@NonNull Task<DriveFolder> task) {
+                //Do stuff
+            }
+        });
     }
 }
